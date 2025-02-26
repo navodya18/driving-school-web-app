865eca2b681a477add919072317f6a910095ee47
navodya18 <navosamarasinghe2002@gmail.com>
2025-02-26 18:22:38 +0530

    Hero section

---
+++ frontend/drivingschoolfrontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero container'>
+      <div className="hero-text">
+      <h1> Welcome To Tharuka Driving School</h1>
+      <p>Learn to drive with confidence! We provide expert training, flexible scheduling, and a supportive learning environment to help you master the road safely and efficiently. Start your journey with us today</p>
+      <button className='btn btn1'>Apply Now</button>
+
+      </div>
+    </div>
+  )
+}
+
+export default Hero
